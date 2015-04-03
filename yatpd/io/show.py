e838e17236e2d6c4923621426da0d39d90bdc9db
brickgao <brickgao@gmail.com>
2015-04-04 01:03:34 +0800

    add show func

---
+++ yatpd/io/show.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+
+import cv2
+import numpy as np
+
+
+def imshow(window_title, img_data):
+    '''show a single image.
+
+    Parameters
+    ----------
+    window_title: str
+      title of the window
+
+    img_data: numpy.ndarray
+      image data
+    '''
+    cv2.show(window_tile, img_data)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
