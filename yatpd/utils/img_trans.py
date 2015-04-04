e6070add1aba7334dd9752b27ee26139bb922eff
brickgao <brickgao@gmail.com>
2015-04-04 15:30:24 +0800

    add DoG support for img_trans
    	修改：     img_trans.py
    
    	修改：     img_trans.py
    	修改：     img_trans.py

--- yatpd/utils/img_trans.py
+++ yatpd/utils/img_trans.py
@@ -12,7 +12,7 @@ def img_trans(img_data, trans_type):
       data of image
 
     trans_type: str
-      gray | LUV | Gabor
+      gray | LUV | Gabor | DoG
     '''
     if trans_type == 'gray':
         return (cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY), )
@@ -31,5 +31,12 @@ def img_trans(img_data, trans_type):
             img_gabor_list.append(cv2.filter2D(img_gray, cv2.CV_32F,
                                                gabor_kernel))
         return tuple(img_gabor_list)
+    elif trans_type == 'DoG':
+        img_gray = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
+        img_gaussian_1 = cv.GaussianBlur(img_gray, (1, 1), 0)
+        img_gaussian_3 = cv.GaussianBlur(img_gray, (3, 3), 0)
+        img_gaussian_5 = cv.GaussianBlur(img_gray, (5, 5), 0)
+        return (img_gaussian_3 - img_gaussian_1,
+                img_gaussian_5 - img_gaussian_3)
     else:
         raise Exception("Type %s trans_type not support" % trans_type)
