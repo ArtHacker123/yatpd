9e3749554aec52d32da9b5d159c8ed4fc0bc016b
brickgao <brickgao@gmail.com>
2015-04-04 12:08:58 +0800

    add timer decorator

---
+++ yatpd/utils/__init__.py
@@ -0,0 +1,4 @@
+# -*- encoding: utf-8 -*-
+
+
+from timer import timer
---
+++ yatpd/utils/timer.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+
+import time
+
+
+def timer(func, *args, **kwds):
+    '''A simple decorator to count time.
+    '''
+    time_begin = time.time()
+    ret = func(*args, **kwds)
+    time_end = time.time()
+    time_cost = time_begin - time_end
+    print 'call %s cost %f second(s)' % (func.__name__, time_cost)
+    return ret
