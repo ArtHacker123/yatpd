c415e71b71e6bab707ca7adb0255d8e510154eb6
brickgao <brickgao@gmail.com>
2015-04-06 20:31:54 +0800

    fix utils.__init__

--- yatpd/utils/__init__.py
+++ yatpd/utils/__init__.py
@@ -6,3 +6,4 @@ from img_trans import img_trans
 from draw import draw
 from random_cut import random_cut, random_cut_list
 from model import save_model, load_model
+from hog2hognmf import hog2hognmf
