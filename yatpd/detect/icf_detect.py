93b375abf478f774dc4a9d8a380d7e33ec2f2f39
brickgao <brickgao@gmail.com>
2015-04-11 22:43:14 +0800

    fix icf_detect

--- yatpd/detect/icf_detect.py
+++ yatpd/detect/icf_detect.py
@@ -35,15 +35,15 @@ def icf_detect(model, img_data, model_size, feature_config):
         img_size = img_data.shape
         pos_list = []
         img_slice_list = []
-        for x in range(0, img_size[0] - model_size[0], 8):
-            for y in range(0, img_size[1] - model_size[1], 8):
+        for x in range(0, img_size[0] - model_size[0], 4):
+            for y in range(0, img_size[1] - model_size[1], 4):
                 pos_list.append((x, y))
                 img_slice = img_data[x:x + model_size[0],
                                      y:y + model_size[1]]
                 img_slice_list.append(img_slice)
-        img_feature_list, _ = generate(img_slice, feature_config)
+        img_feature_list, _ = get_icf_feature(img_slice_list, feature_config)
         img_feature_list = np.array(img_feature_list, dtype=np.float32)
-        result_list = model.predict(feature_list)
+        result_list = model.predict(img_feature_list)
         for flag, pos in zip(result_list, pos_list):
             def pos_trans(x):
                 return int(x * rate)
