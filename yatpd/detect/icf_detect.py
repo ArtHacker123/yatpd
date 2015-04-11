96da2752a9ed77626c7d938edea83612fcf56bf5
brickgao <brickgao@gmail.com>
2015-04-12 00:46:12 +0800

    reduce memory usage

--- yatpd/detect/icf_detect.py
+++ yatpd/detect/icf_detect.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 from ..utils import get_icf_feature
 from ..utils import timer
+from ..utils import draw
 
 
 @timer
@@ -34,14 +35,14 @@ def icf_detect(model, img_data, model_size, feature_config):
     for img_data, rate, color in img_data_list[:2]:
         img_size = img_data.shape
         pos_list = []
-        img_slice_list = []
+        img_feature_list = []
         for x in range(0, img_size[0] - model_size[0], 4):
             for y in range(0, img_size[1] - model_size[1], 4):
                 pos_list.append((x, y))
                 img_slice = img_data[x:x + model_size[0],
                                      y:y + model_size[1]]
-                img_slice_list.append(img_slice)
-        img_feature_list, _ = get_icf_feature(img_slice_list, feature_config)
+                img_feature, _ = get_icf_feature([img_slice], feature_config)
+                img_feature_list += img_feature
         img_feature_list = np.array(img_feature_list, dtype=np.float32)
         result_list = model.predict(img_feature_list)
         for flag, pos in zip(result_list, pos_list):
