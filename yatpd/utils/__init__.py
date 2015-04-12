09a75049e864205aba2fcdee78fb46edb7a58096
brickgao <brickgao@gmail.com>
2015-04-12 23:23:15 +0800

    fix utils.__init__

--- yatpd/utils/__init__.py
+++ yatpd/utils/__init__.py
@@ -8,3 +8,4 @@ from random_cut import random_cut, random_cut_list
 from model import save_model, load_model
 from hog2hognmf import hog2hognmf
 from icf_feature import hog2mats, luv2mats, sobel2mat, get_icf_feature
+from acf_feature import acf_smooth, get_acf_sum, get_acf_feature
